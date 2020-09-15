db7f1d11f234716806696ed341388e0704aaf863
Bojana Belojevic <bojana.belojevic@prodyna.com>
2020-09-15 15:38:06 +0200

    Finishing the course https://app.pluralsight.com/library/courses/angular-2-getting-started-update/table-of-contents

---
+++ APM-Start/src/app/products/product-detail.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from './product.service';
+
+@Component({
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent implements OnInit {
+  pageTitle: string = 'Product Detail';
+  product: IProduct | undefined;
+  errorMessage: string;
+
+  constructor(private route: ActivatedRoute, private router: Router, private productService: ProductService) { }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
+  }
+
+  ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.pageTitle += `: ${id} `;
+
+    this.productService.getProduct(id).subscribe({
+      next: product => this.product = product,
+      error: err => this.errorMessage = err
+    });
+  }
+
+}
